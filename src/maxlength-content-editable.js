f6454507c992b33c7ffaadc319fc5b1d1b5b3642
Unknown <stephen.baroan@gmail.com>
2017-12-21 23:57:35 +0100

    improve compatibility with IE thanks to babel polyfill

--- src/maxlength-content-editable.js
+++ src/maxlength-content-editable.js
@@ -7,57 +7,73 @@
  *    https://github.com/stephen31/maxlength-contenteditable
  *
  */
+/* eslint no-use-before-define:0 */
+
+import 'babel-polyfill';
 
 function maxlengthContentEditable() {
+  const editableElements = document.querySelectorAll('div[contenteditable="true"]');
 
-    const editableElements = document.querySelectorAll('div[contenteditable="true"]');
+  const clipboardEvents = ['copy', 'paste', 'cut'];
+  const keyboardEvents = ['keyup', 'keypress', 'keydown', 'blur', 'change'];
 
-    const clipboardEvents = ['copy', 'paste', 'cut'];
-    const keyboardEvents = ['keyup', 'keypress', 'keydown', 'blur', 'change'];
+  Array.from(editableElements).forEach((element) => {
+    clipboardEvents.forEach((clipboardEvent) => {
+      element.addEventListener(clipboardEvent, clipboardEventHandler);
+    });
+    keyboardEvents.forEach((keyboardEvent) => {
+      element.addEventListener(keyboardEvent, generalEventKeyHandler);
+    });
+  });
 
-    for (let element of editableElements) {
-        for (let event of clipboardEvents) {
-            element.addEventListener(event, clipboardEventHandler);
-        }
-
-        for (let event of keyboardEvents) {
-            element.addEventListener(event, generalEventKeyHandler);
-        }
-    }
-    /**
+  /**
      *callback for 'cut', 'copy' , 'paste' events
      *this function will prevent the basic behavior
       of an event if the size of the text present in the element
       plus the size of the text present in the clipboard exceeds exceeds the maxlength alowed
      * @param {any} event
      */
-    function clipboardEventHandler(event) {
-        //IE
-        if (window.clipboardData) {
-            if (window.clipboardData.getData('Text').length + this.textContent.length > this.dataset.maxLength) {
-                event.preventDefault();
-            }
-        }
-        //Chrome , Firefox
-        if (event.clipboardData) {
-            if (event.clipboardData.getData('Text').length + this.textContent.length > this.dataset.maxLength && event.keyCode != 8) {
-                event.preventDefault();
-            }
-        }
+  function clipboardEventHandler(event) {
+    // IE
+    if (window.clipboardData) {
+      if (window.clipboardData.getData('Text').length + this.textContent.length > this.dataset.maxLength) {
+        event.preventDefault();
+      }
     }
-    /**
+    // Chrome , Firefox
+    if (event.clipboardData) {
+      if (event.clipboardData.getData('Text').length + this.textContent.length > this.dataset.maxLength && event.keyCode !== 8) {
+        event.preventDefault();
+      }
+    }
+  }
+  /**
      *callback for 'keyup', 'keypress', 'keydown', 'blur', 'change' events
      *this function will prevent the basic behavior
       of an event if the size of the text present in the element
       plus the size of the character typed exceeds the maxlength alowed
      * @param {any} event
      */
-    function generalEventKeyHandler(event) {
-        if (this.dataset.maxLength && this.textContent.length == this.dataset.maxLength && event.keyCode != 8 && !event.ctrlKey) {
-            event.preventDefault();
-        }
+  function generalEventKeyHandler(event) {
+    if (this.dataset.maxLength && this.textContent.length == this.dataset.maxLength && !isAllowedKeyCode(event.keyCode) && !event.ctrlKey) {
+      event.preventDefault();
     }
-
+  }
+  /**
+   * Check if a keycode is allowed when max limit is reached
+   * 8 : Backspace
+   * 37: LeftKey
+   * 38: UpKey
+   * 39: RightKey
+   * 40: DownKey
+   * @param {any} eventKeycode
+   * @returns boolean
+   */
+  function isAllowedKeyCode(key) {
+    return key === 8 || key === 38 || key === 39 || key === 37 || key === 40;
+  }
 }
 
-export {maxlengthContentEditable};
+export {
+  maxlengthContentEditable
+};
